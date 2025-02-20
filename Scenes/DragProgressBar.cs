26715f66a8423f8f4419c62daef9753fd5401c8f
AlbedoExe <124367727+AlbedoExe@users.noreply.github.com>
2025-02-20 17:25:32 +0000

    fixed issues with leaving the drag and returning, still dealing with adjacent bar issues, will look into this on the 21st or 22nd

--- Scenes/DragProgressBar.cs
+++ Scenes/DragProgressBar.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Linq;
 
 public partial class DragProgressBar : ProgressBar
 {
@@ -7,21 +8,24 @@ public partial class DragProgressBar : ProgressBar
 	DraggableRect childDragger;
 
     [Export]
-    float cursorDragRange = 80.0f;
+    float cursorDragRange = 20.0f;
     [Export]
     int BarDepletionRate = 15;
 
+    public bool isContinued;
 
-    private Vector2 StartPos;
-    private Vector2 EndPos;
-    public float startDragRange = 50.0f;
+    public Vector2 StartPos;
+    public Vector2 EndPos;
+    public float startDragRange = 40.0f;
     public float progress;
 
     private bool hasStarted = false;
-    private bool isDragging;
+    public bool isDragging;
     private bool isCompleted = false;
 
-
+    Godot.Collections.Array<Node> SceneBars;
+    public InputEvent inputev;
+    private Vector2 progressVec;
     private Vector2 currentStart;
     private Vector2 currentEnd;
 
@@ -32,17 +36,25 @@ public partial class DragProgressBar : ProgressBar
 		Material = shaderMat;
 
         StartPos = this.GlobalPosition;
-        EndPos = StartPos + new Vector2(this.Size.X * this.Scale.X, 0).Rotated(this.Rotation);
-    
+        EndPos = StartPos + new Vector2(this.Size.X * this.Scale.X,0).Rotated(this.Rotation);
+
+        Node root = GetTree().Root.GetChild(0);
+
+        SceneBars = this.GetParent().GetChildren(false);
     }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-
+        GD.Print("PROG: " + progressVec);
         if (progress >= 0.99f)
         {
             isCompleted = true;
+
+            if(SceneBars.Count > 0)
+            {
+                CheckAdjacentBars();
+            }
         }
 
         //if the drag is left without it being completed it begins reseting
@@ -54,12 +66,14 @@ public partial class DragProgressBar : ProgressBar
 
 	public void MouseInput(InputEvent @event)
 	{
+        GD.Print(this.GetInstanceId());
         if (!isCompleted)
         {
-
+            GD.Print("is not completed");
             //checks if the event was a mouse input
             if (@event is InputEventMouseButton mouseButton)
             {
+                GD.Print("is a mouse button");
                 //isdragging is based on the mouse input being left click
                 isDragging = mouseButton.ButtonIndex == MouseButton.Left;
 
@@ -67,11 +81,11 @@ public partial class DragProgressBar : ProgressBar
                 {
                     if((StartPos.DistanceTo(mouseButton.GlobalPosition) < startDragRange) && !hasStarted)
                     {
-                        GD.Print("Started drag from left");
+                        this.Value = 0;
                         this.FillMode = 0;
                         currentStart  = StartPos;
                         currentEnd = EndPos;
-                        shaderMat.SetShaderParameter("isLeft", true);
+                        progressVec = currentStart;
                         hasStarted = true;
                     }
                 }
@@ -79,11 +93,11 @@ public partial class DragProgressBar : ProgressBar
                 {
                     if ((EndPos.DistanceTo(mouseButton.GlobalPosition) < startDragRange) && !hasStarted)
                     {
-                        GD.Print("Started drag from Right");
+                        this.Value = 0;
                         this.FillMode = 1;
                         currentStart = EndPos;
                         currentEnd = StartPos;
-                        shaderMat.SetShaderParameter("isLeft", false);
+                        progressVec = currentStart;
                         hasStarted = true;
                     }
                 }
@@ -94,14 +108,18 @@ public partial class DragProgressBar : ProgressBar
                 }
             }
 
+
             //if there is mouse motion while dragging then drag the object for progress
-            if (@event is InputEventMouseMotion mouseMotion && isDragging)
+            if (@event is InputEventMouseMotion mouseMotion && isDragging && IsWithinDragRange(mouseMotion.GlobalPosition))
             {
- 
+                GD.Print("is mouse motion");
                 //progress is the dist of the mouse position along the path between start and end positions of the progress bar
                 // returned value is clamped between 0 and 1 for lerp
                 progress = Mathf.Clamp(ProgressAlongBar(mouseMotion.GlobalPosition), 0.0f, 1.0f);
 
+                //progress vector for keeping the mouse in range of the drag
+                progressVec = currentStart.Lerp(currentEnd, progress);
+                
                 //updates progress bar
                 Value = progress * MaxValue;
 
@@ -110,19 +128,16 @@ public partial class DragProgressBar : ProgressBar
                     float progressVal = (float)Value / (float)MaxValue;
                     shaderMat.SetShaderParameter("progress", progressVal);
                 }
-
-
-                //checks if within range so you cant drag from anywhere on screen
-                //TODO: FIX THIS 
-                //if (!IsWithinDragRange(mouseMotion.GlobalPosition))
-                //{
-                //    GD.Print("LeavingDragArea");
-                //    isDragging = false;
-                //}
             }
+
+
         }
     }
 
+    public void MouseEnteredSpace()
+    {
+
+    }
     //function for calculating the distance of the mousePos along the path between endPos and StartPos
     //Projects mousePos along path to find the distance and progress along the path at that time
     private float ProgressAlongBar(Vector2 MousePos)
@@ -138,14 +153,18 @@ public partial class DragProgressBar : ProgressBar
     //checks if the mouse position is within cursorDragRange
     private bool IsWithinDragRange(Vector2 MousePos)
     {
-        float dist = GlobalPosition.DistanceTo(MousePos);
-
+  
+        float dist = MousePos.DistanceTo(progressVec);
+        GD.Print("vec1: " + progressVec);
+        GD.Print("vec2: " + MousePos);
         if (dist <= cursorDragRange)
         {
+            GD.Print("within range");
             return true;
         }
         else
         {
+            GD.Print("Not within range");
             return false;
         }
     }
@@ -155,11 +174,30 @@ public partial class DragProgressBar : ProgressBar
     {
         Value -= (BarDepletionRate * delta);
 
-        //this.GlobalPosition = currentStart.Lerp(currentEnd, (float)Value / (float)MaxValue);
+        progressVec = currentStart.Lerp(currentEnd, (float)Value / (float)MaxValue);
 
         if(Value < 20.0f)
         {
             hasStarted = false;
+        }
+    }
+
+    private void CheckAdjacentBars()
+    {
+        DragProgressBar currentBarIndex;
+        for (int i = 0; i < SceneBars.Count; i++)
+        {
+            currentBarIndex = SceneBars[i] as DragProgressBar;
+
+            if ((isDragging) && currentBarIndex.EndPos.DistanceTo(currentEnd) < 10 || currentBarIndex.StartPos.DistanceTo(currentEnd) < 10)
+            {
+                GD.Print("Valid adjacent bar");
+                if (isCompleted) 
+                {
+                
+                }
+
+            }
         }
     }
 }
