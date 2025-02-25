65c153e4562f786851166bcada2a2ffd67d34dcc
ErythiaSea <124370780+ErythiaSea@users.noreply.github.com>
2025-02-25 11:33:57 +0000

    begin ladder implementation

--- Scripts/GravPlayer.cs
+++ Scripts/GravPlayer.cs
@@ -1,10 +1,18 @@
 using Godot;
 using System;
+using static Godot.TextServer;
 
 public partial class GravPlayer : CharacterBody2D
 {
 	public const float Speed = 300.0f;
 	public const float JumpVelocity = -400.0f;
+
+	// possible todo: enum with "player states" instead of millions of bools
+	// however im short-sighted and can't figure out what other states i need,
+	// which ones need to be mutually exclusive, etc. this is fine for rn :3
+	public bool onLadder = false;
+
+	public Vector2 moveTo = Vector2.Zero;
 
 	AnimatedSprite2D playerSprite;
 
@@ -17,6 +25,16 @@ public partial class GravPlayer : CharacterBody2D
     }
 
     public override void _PhysicsProcess(double delta)
+	{
+		if (moveTo != Vector2.Zero)
+		{
+
+		}
+		if (!onLadder) standardMovement(delta);
+		else ladderMovement(delta);
+	}
+
+	void standardMovement(double delta)
 	{
 		Vector2 velocity = Velocity;
 
@@ -33,8 +51,6 @@ public partial class GravPlayer : CharacterBody2D
 		}
 
 		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-
 		//Vector2 direction = Input.GetVector("left_pivot_cw", "left_pivot_ccw", "up", "down");
 		float xDirection = Input.GetAxis("left_pivot_cw", "left_pivot_ccw");
 		if (xDirection != 0)
@@ -48,16 +64,34 @@ public partial class GravPlayer : CharacterBody2D
 
 		Velocity = velocity;
 
-        if (Velocity != Vector2.Zero)
-        {
-            playerSprite.Play();
-            if (Velocity.X != 0) playerSprite.FlipH = Velocity.X > 0;
-        }
-        else
-        {
-            playerSprite.Frame = 0;
-            playerSprite.Pause();
-        }
-        MoveAndSlide();
+		// Animate player sprite based on the velocity of the player
+		if (Velocity != Vector2.Zero)
+		{
+			playerSprite.Play();
+			if (Velocity.X != 0) playerSprite.FlipH = Velocity.X > 0;
+		}
+		else
+		{
+			playerSprite.Frame = 0;
+			playerSprite.Pause();
+		}
+		MoveAndSlide();
+	}
+
+	private void ladderMovement(double delta)
+	{
+		Vector2 velocity = Vector2.Zero;
+		float yDirection = Input.GetAxis("up", "down");
+		if (yDirection != 0)
+		{
+			velocity.Y = yDirection * Speed;
+		}
+		else
+		{
+			velocity.Y = Mathf.MoveToward(Velocity.Y, 0, Speed);
+		}
+
+		//Velocity = velocity;
+		MoveAndCollide(velocity);
 	}
 }
