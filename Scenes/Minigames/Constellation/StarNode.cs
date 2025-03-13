0ea6a902820fc18e2f4219f57b92f681f99085ca
AlbedoExe <124367727+AlbedoExe@users.noreply.github.com>
2025-03-13 12:27:25 +0000

    Star nodes and star group objs

---
+++ Scenes/Minigames/Constellation/StarNode.cs
@@ -0,0 +1,117 @@
+using Godot;
+using System;
+
+public partial class StarNode : Node2D
+{
+    [Export]
+    Godot.Collections.Array<StarNode> adjacentStars;
+
+    [Export]
+    float centerFocusRange = 200f;
+
+    [Export]
+    float lineSpeed = 2.0f;
+
+    private float timeToRegister = 2f;
+    private float timeElapsed = 0f;
+    private float alphaChange = 0f;
+
+
+    private Godot.Collections.Array<Line2D> lineList;
+    private Godot.Collections.Array<Vector2> lineTargets;
+    private Godot.Collections.Array<float> lineProgress;
+
+    private StarsParent parent;
+    private Label numberDisplay;
+
+    public bool isFound = false;
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        numberDisplay = GetChild(1) as Label;
+        parent = GetParent() as StarsParent;
+        lineList = new Godot.Collections.Array<Line2D>();
+        lineTargets = new Godot.Collections.Array<Vector2>();
+        lineProgress = new Godot.Collections.Array<float>();
+        timeElapsed = 0f;
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+        if (!isFound)
+        {
+            if (GlobalPosition.DistanceTo(GetViewport().GetCamera2D().GetScreenCenterPosition()) < centerFocusRange)
+            {
+                timeElapsed += (float)delta;
+
+                if (timeElapsed > timeToRegister)
+                {
+                    GD.Print("Star found");
+                    isFound = true;
+                    StarFound();
+                }
+
+            }
+        }
+        else
+        {
+            if (alphaChange <= 1)
+            {
+                GD.Print("Displaying Number");
+                alphaChange += (float)delta * parent.displaySpeed;
+                numberDisplay.Modulate = new Color(numberDisplay.Modulate.R, numberDisplay.Modulate.G, numberDisplay.Modulate.B, alphaChange);
+            }
+        }
+
+
+        if (lineList.Count > 0)
+        {
+            GD.Print("line exists");
+            for(int i = 0; i < lineList.Count; i++)
+            {
+                if (lineProgress[i] < 1f)
+                {
+                    GD.Print("Expanding");
+                    lineProgress[i] += (float)delta * lineSpeed;
+
+                    Mathf.Clamp(lineProgress[i], 0f, 1f);
+
+                    Vector2 linePos = lineList[i].Points[0].Lerp(lineTargets[i], lineProgress[i]);
+
+                    lineList[i].SetPointPosition(1, linePos);
+                }
+            }
+        }
+
+
+    }
+
+    public void ConnectStars(Node2D star)
+    {
+   
+        Line2D connection = new Line2D();
+
+        connection.AddPoint(GlobalPosition);
+        connection.AddPoint(GlobalPosition);
+ 
+        connection.Width = 4;
+        connection.DefaultColor = Colors.White;
+        this.AddSibling(connection);
+
+       
+        lineList.Add(connection);
+        lineProgress.Add(0f);
+        lineTargets.Add(star.GlobalPosition);
+    }
+    private void StarFound()
+    {
+        foreach (StarNode star in adjacentStars)
+        {
+            if (star.isFound)
+            {
+                ConnectStars(star);
+            }
+        }
+    }
+}
