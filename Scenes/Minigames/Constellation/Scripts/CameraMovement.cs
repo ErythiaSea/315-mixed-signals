0a7bde952df939b4730834967af3455795836648
RuaraidhCric <159905805+RuaraidhCric@users.noreply.github.com>
2025-03-20 17:33:20 +0000

    Constellation Reveal Audio

--- Scenes/Minigames/Constellation/Scripts/CameraMovement.cs
+++ Scenes/Minigames/Constellation/Scripts/CameraMovement.cs
@@ -3,62 +3,89 @@ using System;
 
 public partial class CameraMovement : Camera2D
 {
-    [Export]
-    public float cameraSpeed = 0.6f;
+	[Export]
+	public float cameraSpeed = 0.6f;
 
-    private bool canMoveCam = true;
-    private ColorRect telescope;
-    private Vector2 center;
+	private bool canMoveCam = true;
+	private ColorRect telescope;
+	private Vector2 center;
+	private AudioStreamPlayer audioPlayer;
 
-    Globals globalScript;
+	Globals globalScript;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-        globalScript = Globals.Instance;
-        telescope = GetNode("Telescope") as ColorRect;
+		globalScript = Globals.Instance;
+		telescope = GetNode("Telescope") as ColorRect;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-        if(canMoveCam)CameraInputEvents(delta);
+		if(canMoveCam)CameraInputEvents(delta);
 
-        center = GetScreenCenterPosition();
-    }
+		center = GetScreenCenterPosition();
+	}
 
 	public void CameraInputEvents(double delta)
 	{
-        if (Input.IsActionPressed("down"))
-        {
+		if (Input.IsActionPressed("down"))
+		{
 			Position = new Vector2(Position.X, Position.Y + cameraSpeed);
-        }
+		}
 		if (Input.IsActionPressed("up"))
 		{
-            Position = new Vector2(Position.X, Position.Y - cameraSpeed);
-        }
-        if (Input.IsActionPressed("left"))
-        {
-            Position = new Vector2(Position.X - cameraSpeed, Position.Y);
-        }
-        if (Input.IsActionPressed("right"))
-        {
-            Position = new Vector2(Position.X + cameraSpeed, Position.Y);
-        }
-    }
+			Position = new Vector2(Position.X, Position.Y - cameraSpeed);
+		}
+		if (Input.IsActionPressed("left"))
+		{
+			Position = new Vector2(Position.X - cameraSpeed, Position.Y);
+		}
+		if (Input.IsActionPressed("right"))
+		{
+			Position = new Vector2(Position.X + cameraSpeed, Position.Y);
+		}
+	}
+	
+	private void DisplayConstellation(Vector2 centerStar)
+	{
+	// Get the AudioStreamPlayer node (adjust the path if needed)
+	AudioStreamPlayer audioPlayer = GetNode<AudioStreamPlayer>("AudioStreamPlayer");
 
-    private void DisplayConstellation(Vector2 centerStar)
-    {
-        canMoveCam = false;
-        Tween completetion = GetTree().CreateTween();
-       
-        completetion.Parallel().TweenProperty(this, "zoom", new Vector2(0.4f, 0.4f), 2f);
-        completetion.Parallel().TweenProperty(this, "position", centerStar, 1f);
-        completetion.Parallel().TweenProperty(telescope, "scale", new Vector2(2.5f,2.5f), 2f);
-        completetion.TweenCallback(Callable.From(this.UpdateGlobals));
-    }
+	// Load the "Reveal" sound and play it
+	if (audioPlayer != null)
+	{
+		GD.Print("AudioStreamPlayer found!");
+		
+		var revealSound = (AudioStream)GD.Load("res://Scenes/Minigames/Constellation/Scripts/Reveal.wav");
+		if (revealSound != null)
+		{
+						audioPlayer.Stream = revealSound;
+			audioPlayer.VolumeDb = 0; // Ensure it's audible
+			audioPlayer.Play();
+			GD.Print("Playing Reveal sound...");
+		}
+		else
+		{
+			GD.PrintErr("Failed to load Reveal sound. Check the file path.");
+		}
+	}
+	else
+	{
+		GD.PrintErr("AudioStreamPlayer node not found! Check the node path.");
+	}
+	
+		canMoveCam = false;
+		Tween completetion = GetTree().CreateTween();
+	   
+		completetion.Parallel().TweenProperty(this, "zoom", new Vector2(0.4f, 0.4f), 6f); //needs to be at least 5.6 seconds for reveal audio
+		completetion.Parallel().TweenProperty(this, "position", centerStar, 1f);
+		completetion.Parallel().TweenProperty(telescope, "scale", new Vector2(2.5f,2.5f), 6f); //needs to be at least 5.6 seconds for reveal audio
+		completetion.TweenCallback(Callable.From(this.UpdateGlobals));
+	}
 
-    private void UpdateGlobals()
-    {
-        globalScript.gameState.stage = GAMESTAGE.END;
-    }
+	private void UpdateGlobals()
+	{
+		globalScript.gameState.stage = GAMESTAGE.END;
+	}
 }
--- Scenes/Minigames/Constellation/Scripts/StarsParent.cs
+++ Scenes/Minigames/Constellation/Scripts/StarsParent.cs
@@ -6,134 +6,134 @@ using System.Reflection;
 
 public partial class StarsParent : Node2D
 {
-    [Signal]
-    public delegate void ConstellationCompletionEventHandler(Vector2 centerStar);
+	[Signal]
+	public delegate void ConstellationCompletionEventHandler(Vector2 centerStar);
 
-    [Export]
-    int randMin = -4;
+	[Export]
+	int randMin = -4;
 
-    [Export]
-    int randMax = 4;
+	[Export]
+	int randMax = 4;
 
-    [Export]
-    public float displaySpeed = 0.4f;
+	[Export]
+	public float displaySpeed = 0.4f;
 
 
-    public bool hasSignalled = false;
-    // Called when the node enters the scene tree for the first time.
+	public bool hasSignalled = false;
+	// Called when the node enters the scene tree for the first time.
 
-    private Godot.Collections.Array<Label> starLabels;
+	private Godot.Collections.Array<Label> starLabels;
 
-    private Godot.Collections.Array<StarNode> stars;
+	private Godot.Collections.Array<StarNode> stars;
 
 	public override void _Ready()
 	{
-        stars = GetStars();
-        starLabels = GetLabels();
-        GD.Print(stars.Count);
-        DisplayNumbers();
+		stars = GetStars();
+		starLabels = GetLabels();
+		GD.Print(stars.Count);
+		DisplayNumbers();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 
-        if (IsConstellationComplete() && !hasSignalled)
-        {
-            EmitSignal(SignalName.ConstellationCompletion, GetCenterStar());
-            hasSignalled = true;
-        }
-              
-    }
-    public void DisplayNumbers()
-    {
-        Godot.Collections.Array<int> randNumArray = RandomNumbers();
+		if (IsConstellationComplete() && !hasSignalled)
+		{
+			EmitSignal(SignalName.ConstellationCompletion, GetCenterStar());
+			hasSignalled = true;
+		}
+			  
+	}
+	public void DisplayNumbers()
+	{
+		Godot.Collections.Array<int> randNumArray = RandomNumbers();
 
-        for (int i = 0; i < starLabels.Count; i++)
-        {
-            Label lbl = starLabels[i] as Label;
-            lbl.Text = string.Empty;
-            lbl.Text = randNumArray[i].ToString();
-        }
-    }
+		for (int i = 0; i < starLabels.Count; i++)
+		{
+			Label lbl = starLabels[i] as Label;
+			lbl.Text = string.Empty;
+			lbl.Text = randNumArray[i].ToString();
+		}
+	}
 
-    private Godot.Collections.Array<int> RandomNumbers()
-    {
-        Godot.Collections.Array<int> randNumArray = new Godot.Collections.Array<int>();
-        do
-        {
-            randNumArray.Clear();
+	private Godot.Collections.Array<int> RandomNumbers()
+	{
+		Godot.Collections.Array<int> randNumArray = new Godot.Collections.Array<int>();
+		do
+		{
+			randNumArray.Clear();
 
-            for (int i = 0; i < starLabels.Count; i++)
-            {
-                var rng = new RandomNumberGenerator();
-                int randomNum = rng.RandiRange(randMin, randMax);
+			for (int i = 0; i < starLabels.Count; i++)
+			{
+				var rng = new RandomNumberGenerator();
+				int randomNum = rng.RandiRange(randMin, randMax);
 
-                randNumArray.Add(randomNum);
-            }
+				randNumArray.Add(randomNum);
+			}
 
-        } while (randNumArray.Sum() == 0 && randNumArray.Sum() < -4 && randNumArray.Sum() > 4);
+		} while (randNumArray.Sum() == 0 && randNumArray.Sum() < -4 && randNumArray.Sum() > 4);
 
-        GD.Print("Valid random numbers");
+		GD.Print("Valid random numbers");
 
-        //updates global variable for cipher key in translation mechanic
-        Globals.Instance.cipherKey = randNumArray.Sum();
-        return randNumArray;
+		//updates global variable for cipher key in translation mechanic
+		Globals.Instance.cipherKey = randNumArray.Sum();
+		return randNumArray;
 
-    }
-    
-    private bool IsConstellationComplete()
-    {
-        int starsCompleted = 0;
+	}
+	
+	private bool IsConstellationComplete()
+	{
+		int starsCompleted = 0;
 
-        for(int i = 0; i < stars.Count; i++)
-        {
-            if (stars[i].isFound)
-            {
-                starsCompleted++;
-            }
-        }
+		for(int i = 0; i < stars.Count; i++)
+		{
+			if (stars[i].isFound)
+			{
+				starsCompleted++;
+			}
+		}
 
-        if (starsCompleted == stars.Count) return true;
+		if (starsCompleted == stars.Count) return true;
 
-        return false;
-    }
-    private Godot.Collections.Array<Label> GetLabels()
-    {
-        Godot.Collections.Array<Label> lbls = new Godot.Collections.Array<Label>();
-        foreach (Node2D child in GetChildren())
-        {
-            Label current = child.GetChild(2) as Label;
-            current.Modulate = new Color(current.Modulate.R, current.Modulate.G, current.Modulate.B, 0);
-            lbls.Add(current);
-        }
+		return false;
+	}
+	private Godot.Collections.Array<Label> GetLabels()
+	{
+		Godot.Collections.Array<Label> lbls = new Godot.Collections.Array<Label>();
+		foreach (Node2D child in GetChildren())
+		{
+			Label current = child.GetChild(2) as Label;
+			current.Modulate = new Color(current.Modulate.R, current.Modulate.G, current.Modulate.B, 0);
+			lbls.Add(current);
+		}
 
-        return lbls;
-    }
+		return lbls;
+	}
 
-    private Godot.Collections.Array<StarNode> GetStars()
-    {
-        Godot.Collections.Array<StarNode> strs = new Godot.Collections.Array<StarNode>();
-        foreach (Node2D child in GetChildren())
-        {
-            strs.Add(child as StarNode);
-        }
+	private Godot.Collections.Array<StarNode> GetStars()
+	{
+		Godot.Collections.Array<StarNode> strs = new Godot.Collections.Array<StarNode>();
+		foreach (Node2D child in GetChildren())
+		{
+			strs.Add(child as StarNode);
+		}
 
-        return strs;
-    }
-    private Vector2 GetCenterStar()
-    {
-        int bestCount = 0;
-        StarNode bestStar = null;
-        for (int i = 0; i < stars.Count; i++)
-        {
-            if (stars[i].adjacentStars.Count > bestCount)
-            {
-                bestCount = stars[i].adjacentStars.Count;
-                bestStar = stars[i];
-            }
-        }
+		return strs;
+	}
+	private Vector2 GetCenterStar()
+	{
+		int bestCount = 0;
+		StarNode bestStar = null;
+		for (int i = 0; i < stars.Count; i++)
+		{
+			if (stars[i].adjacentStars.Count > bestCount)
+			{
+				bestCount = stars[i].adjacentStars.Count;
+				bestStar = stars[i];
+			}
+		}
 
-        return bestStar.GlobalPosition;
-    }
+		return bestStar.GlobalPosition;
+	}
 }
