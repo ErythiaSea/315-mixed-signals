ef59b36e2b247f69d2452ac27a35c23b4d4571e4
Micah :) <164635101+OR11N@users.noreply.github.com>
2025-03-26 19:14:02 +0000

    Corvus Dialogue (& misc changes)
    sorry in advance

--- Scenes/Minigames/Transpond/Scripts/WaveformGame.cs
+++ Scenes/Minigames/Transpond/Scripts/WaveformGame.cs
@@ -6,55 +6,55 @@ public partial class WaveformGame : Node2D
 	WaveRender playerWave, realWave, ghostWave;
 	float targetWavelength, targetAmplitude;
 
-    Globals globalScript;
-    [Export]
-    float ampTolerance = 2.0f;
-    [Export]
-    float wlTolerance = 2.0f;
-    float alignedTimer = 0.0f;
-    bool tunedSignal = false;
+	Globals globalScript;
+	[Export]
+	float ampTolerance = 2.0f;
+	[Export]
+	float wlTolerance = 2.0f;
+	float alignedTimer = 0.0f;
+	bool tunedSignal = false;
 
 	public bool gameActive = false;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-        globalScript = Globals.Instance;
-        playerWave = GetNode<WaveRender>("playerwave");
-        realWave = GetNode<WaveRender>("realwave");
-        ghostWave = GetNode<WaveRender>("ghostwave");
-        newWavelength();
-    }
+		globalScript = Globals.Instance;
+		playerWave = GetNode<WaveRender>("playerwave");
+		realWave = GetNode<WaveRender>("realwave");
+		ghostWave = GetNode<WaveRender>("ghostwave");
+		newWavelength();
+	}
 
-    void newWavelength()
-    {
-        if(globalScript.waveAmpRef == 0f)
-        {
-            targetWavelength = (GD.Randf() * 42.5f) + 7.5f;
-            realWave.wavelength = targetWavelength;
-            ghostWave.wavelength = targetWavelength;
+	void newWavelength()
+	{
+		if(globalScript.waveAmpRef == 0f)
+		{
+			targetWavelength = (GD.Randf() * 42.5f) + 7.5f;
+			realWave.wavelength = targetWavelength;
+			ghostWave.wavelength = targetWavelength;
 
-            targetAmplitude = (GD.Randf() * 115.0f) + 10.0f;
-            realWave.amplitude = targetAmplitude;
-            ghostWave.amplitude = targetAmplitude;
-            tunedSignal = false; alignedTimer = 0.0f;
-        }
-        else
-        {
-            realWave.wavelength = globalScript.waveLenRef;
-            realWave.amplitude = globalScript.waveAmpRef;
+			targetAmplitude = (GD.Randf() * 115.0f) + 10.0f;
+			realWave.amplitude = targetAmplitude;
+			ghostWave.amplitude = targetAmplitude;
+			tunedSignal = false; alignedTimer = 0.0f;
+		}
+		else
+		{
+			realWave.wavelength = globalScript.waveLenRef;
+			realWave.amplitude = globalScript.waveAmpRef;
 
-            playerWave.wavelength = globalScript.waveLenRef;
-            playerWave.amplitude = globalScript.waveAmpRef;
-            ghostWave.Visible = false;
-        }
-    }
+			playerWave.wavelength = globalScript.waveLenRef;
+			playerWave.amplitude = globalScript.waveAmpRef;
+			ghostWave.Visible = false;
+		}
+	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-        GD.Print("waveform active:", gameActive);
-        if (globalScript.gameState.stage != GAMESTAGE.WAVEFORM || !gameActive) return;
+		GD.Print("waveform active:", gameActive);
+		if (globalScript.gameState.stage != GAMESTAGE.WAVEFORM || !gameActive) return;
 
 		float wlChange = 0.5f;
 		float wlMult = (playerWave.wavelength / 100.0f);
@@ -85,27 +85,27 @@ public partial class WaveformGame : Node2D
 			GD.Print(Mathf.Abs(playerWave.wavelength - targetWavelength));
 		}
 
-        if (tunedSignal)
-        {
-            GD.Print("Task complete!");
+		if (tunedSignal)
+		{
+			GD.Print("Task complete!");
 
-            globalScript.waveAmpRef = realWave.amplitude;
-            globalScript.waveLenRef = realWave.wavelength;
-            ghostWave.Visible = false;
-            //Updates the stage that the player is on
-            globalScript.gameState.stage = GAMESTAGE.CONSTELLATION;
-            // show complete text
-            Label wintext = GetNode<Label>("WinText");
-            wintext.Visible = true;
-            playerWave.waveColor = Colors.Green;
-            gameActive = false;
-        }
+			globalScript.waveAmpRef = realWave.amplitude;
+			globalScript.waveLenRef = realWave.wavelength;
+			ghostWave.Visible = false;
+			//Updates the stage that the player is on
+			globalScript.gameState.stage = GAMESTAGE.CONSTELLATION;
+			// show complete text
+			Label wintext = GetNode<Label>("WinText");
+			wintext.Visible = true;
+			playerWave.waveColor = Colors.Green;
+			gameActive = false;
+		}
 
-        if (Mathf.Abs(playerWave.wavelength - targetWavelength) < wlTolerance && Mathf.Abs(playerWave.amplitude - targetAmplitude) < ampTolerance)
-        {
-            alignedTimer += (float)delta;
-            if (alignedTimer > 1.0f) tunedSignal = true;
-        }
-        else alignedTimer = 0.0f;
+		if (Mathf.Abs(playerWave.wavelength - targetWavelength) < wlTolerance && Mathf.Abs(playerWave.amplitude - targetAmplitude) < ampTolerance)
+		{
+			alignedTimer += (float)delta;
+			if (alignedTimer > 1.0f) tunedSignal = true;
+		}
+		else alignedTimer = 0.0f;
 	}
 }
