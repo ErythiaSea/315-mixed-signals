3619c8848f5c557de535a9653fb6c9a2941a77a0
ErythiaSea <124370780+ErythiaSea@users.noreply.github.com>
2025-04-24 09:21:33 +0100

    functional main menu

--- Scenes/Menu/MainMenu/MainMenu.cs
+++ Scenes/Menu/MainMenu/MainMenu.cs
@@ -13,9 +13,14 @@ public partial class MainMenu : Control
 	[Export(PropertyHint.File, "*.tscn")]
 	PackedScene optionsScene;
 
-	Button startButton, optionsButton, quitButton;
+	Control topPage, quitPage, creditPage;
+	Control currentPage;
+
+	Button startButton, optionsButton, creditsButton, quitMenuButton;
+	Button quitGameButton, quitBackButton, creditBackButton;
+
 	List<Button> buttons = new List<Button>{};
-	double quitTimer = 0;
+	Stack<Control> focusStack;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
@@ -23,45 +28,78 @@ public partial class MainMenu : Control
 		// main menu should never be on top of something so we're safe to set the gamestate
 		Globals.SetGamestate(GAMESTATE.MENU);
 
-		startButton = GetNode<Button>("Margins/ButtonContainer/StartButton");
+		topPage = GetNode<Control>("Top");
+		creditPage = GetNode<Control>("Credits");
+		quitPage = GetNode<Control>("Quit");
+
+		currentPage = topPage;
+
+		startButton = topPage.GetNode<Button>("ButtonContainer/StartButton");
 		startButton.GrabFocus();
 		startButton.Pressed += _On_StartButton_Pressed;
 
-		optionsButton = GetNode<Button>("Margins/ButtonContainer/OptionsButton");
+		optionsButton = topPage.GetNode<Button>("ButtonContainer/OptionsButton");
 		optionsButton.Pressed += _On_OptionsButton_Pressed;
 
-		quitButton = GetNode<Button>("Margins/ButtonContainer/QuitButton");
+		creditsButton = topPage.GetNode<Button>("ButtonContainer/CreditsButton");
+		creditBackButton = creditPage.GetNode<Button>("BackButton");
+		creditsButton.Pressed += () => ForwardPage(creditPage, creditBackButton);
+		creditBackButton.Pressed += BackwardPage;
+
+		quitMenuButton = topPage.GetNode<Button>("ButtonContainer/QuitButton");
+		quitGameButton = quitPage.GetNode<Button>("QuitButton");
+
+		quitMenuButton.Pressed += () => ForwardPage(quitPage, quitGameButton);
+		quitGameButton.Pressed += QueueFree;
+		quitPage.GetNode<Button>("BackButton").Pressed += BackwardPage;
 
 		buttons.Add(startButton);
-		buttons.Add(quitButton);
+		buttons.Add(quitMenuButton);
 		buttons.Add(optionsButton);
 
 		if (optionsScene == null)
 		{
 			optionsScene = (PackedScene)ResourceLoader.Load("res://Scenes/Menu/Options/Options.tscn");
 		}
+
+		// create the focus stack
+		focusStack = new Stack<Control>();
+	}
+
+	private void ForwardPage(Control page, Control focusReceiver)
+	{
+		// Add the button that opens a new page to the focus stack
+		focusStack.Push(GetViewport().GuiGetFocusOwner());
+
+		// hide the previous page, show the new page and set as current
+		currentPage.Visible = false;
+		page.Visible = true;
+		currentPage = page;
+
+		// the first button of the new page will get focus
+		focusReceiver.GrabFocus();
+	}
+
+	private void BackwardPage()
+	{
+		// hide the previous page, show the new page and set as current
+		currentPage.Visible = false;
+		topPage.Visible = true;
+		currentPage = topPage;
+
+		// return focus to whatever button put us on that page
+		focusStack.Pop().GrabFocus();
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		// quit button stuff
-		if (quitButton.ButtonPressed)
+		// Go back if the back button is pressed and we're not on the top page
+		if (Input.IsActionJustPressed("ui_cancel"))
 		{
-			quitTimer += delta;
-			if (quitTimer > 0.5)
+			if (currentPage != topPage)
 			{
-				GetTree().Quit();
-			}
-		}
-		else { quitTimer = 0; }
-
-		// each button grabs and holds focus if mouse is over it (disables keyboard/controller control)
-		foreach (Button button in buttons)
-		{
-			if (button.IsHovered())
-			{
-				button.GrabFocus();
+				BackwardPage();
 			}
 		}
 	}
