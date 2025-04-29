7963b51f757014545921c2dc0968abffa157cdd3
Eryth <124370780+ErythiaSea@users.noreply.github.com>
2025-04-29 18:04:31 +0100

    spoof ui input echo for translation keyboard
    also add license for dialogue nodes

--- Scenes/Minigames/Translation/Keyboard.cs
+++ Scenes/Minigames/Translation/Keyboard.cs
@@ -13,10 +13,17 @@ public partial class Keyboard : HBoxContainer
 	Button deleteButton;
 
 	public Button firstButton;
+	Timer timer;
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		timer = new Timer();
+		AddChild(timer);
+		timer.WaitTime = 0.5;
+		timer.OneShot = true;
+        timer.Timeout += OnTimerTimeout;
+
 		firstButton = new();
 		for (int i = 0; i < 26; i++)
 		{
@@ -65,8 +72,44 @@ public partial class Keyboard : HBoxContainer
 		}
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+    private void OnTimerTimeout()
+    {
+		timer.WaitTime = 0.5;
+		Control focusOwner = GetViewport().GuiGetFocusOwner();
+        if (focusOwner.GetParent() == this)
+		{
+			if (Input.IsActionPressed("ui_left"))
+			{
+				timer.WaitTime = 0.125;
+				timer.Start();
+				Control left = focusOwner.GetNode(focusOwner.FocusNeighborLeft) as Control;
+				left.GrabFocus();
+				return;
+			}
+			else if (Input.IsActionPressed("ui_left"))
+			{
+                timer.WaitTime = 0.125;
+                timer.Start();
+                Control right = focusOwner.GetNode(focusOwner.FocusNeighborRight) as Control;
+				right.GrabFocus();
+				return;
+			}
+		}
+	}
+
+	public override void _GuiInput(InputEvent @event)
+	{
+		if (@event.IsActionPressed("ui_left") || @event.IsActionPressed("ui_right"))
+		{
+			if (GetViewport().GuiGetFocusOwner().GetParent() == this)
+			{
+				timer.Start();
+			}
+		}
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
 	{
 	}
 
